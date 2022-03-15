c85a509a5dfc045d293a45aebb3a4850472e547d
Achille Roussel <achille@segment.com>
2022-03-15 11:47:11 -0700

    remove dependency on constraints package

--- compare/compare.go
+++ compare/compare.go
@@ -1,9 +1,13 @@
 package compare
 
-import "constraints"
+// Ordered is a type constraint enumerating primitive types that support the
+// "<" and ">" operators.
+type Ordered interface {
+	~int8 | ~int16 | ~int32 | ~int64 | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64 | ~string
+}
 
 // Function is a comparison function for ordered types.
-func Function[T constraints.Ordered](a, b T) int {
+func Function[T Ordered](a, b T) int {
 	switch {
 	case a < b:
 		return -1
