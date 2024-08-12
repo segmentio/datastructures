f2a124eb57548a648d1c73e0628302b1b0b93cb1
Jeremy Larkin <jlarkin@twilio.com>
2024-08-12 09:33:55 -0700

    add queue backed by an array

---
+++ container/queue/queue.go
@@ -0,0 +1,71 @@
+package queue
+
+import "github.com/segmentio/datastructures/v2/container/array"
+
+// Queue implements a FIFO queue backed by an array.Array for efficient
+// block-based allocations.
+type Queue[T any] struct {
+	values array.Array[T]
+	index  int
+}
+
+// Len gets the number of values in the queue.
+func (q *Queue[T]) Len() int {
+	return q.values.Len() - q.index
+}
+
+// Empty tests if the Queue has no entries.
+func (q *Queue[T]) Empty() bool {
+	return q.values.Len() == q.index
+}
+
+// Get obtains the value of an entry at a specific offset. Like a Go slice
+// this results in a panic if the value is outside the array bounds.
+func (q *Queue[T]) Get(i int) T {
+	return q.values.Get(i + q.index)
+}
+
+// Ptr obtains the pointer to a value for an entry at a specific offset. The
+// returned pointer is guaranteed not to change until the index is popped from
+// the array. Like a Go slice this results in a panic if the value is outside
+// the array bounds.
+func (q *Queue[T]) Ptr(i int) *T {
+	return q.values.Ptr(i + q.index)
+}
+
+// Push adds a new value to the end of the queue.
+func (q *Queue[T]) Push(v T) {
+	q.PushWithPool(v, nil)
+}
+
+// PushWithPool adds a new value to the end of the queue. If a new block is
+// required by the backing array.Array, it will be allocated from the pool.
+func (q *Queue[T]) PushWithPool(v T, pool *array.Pool[T]) {
+	q.values.PushWithPool(v, pool)
+}
+
+// Pop removes the oldest value from the queue in FIFO order if any are available.
+func (q *Queue[T]) Pop() (v T, ok bool) {
+	return q.PopWithPool(nil)
+}
+
+// PopWithPool removes the oldest value from the queue in FIFO order if any are
+// available. If the removed value was the only item remaining in the block of
+// the backing array.Array, that block will be returned to the pool.
+func (q *Queue[T]) PopWithPool(pool *array.Pool[T]) (v T, ok bool) {
+	if i, n := q.index, q.values.Len(); i < n {
+		ptr := q.values.Ptr(i)
+		v, ok, i = *ptr, true, i+1
+
+		var zero T
+		*ptr = zero // zero out as we go to avoid clear possible memory references
+
+		if i == array.BlockLen[T]() {
+			q.values.ShiftBlocksWithPool(1, pool)
+			i = 0
+		}
+
+		q.index = i
+	}
+	return
+}
---
+++ container/queue/queue_test.go
@@ -0,0 +1,79 @@
+package queue
+
+import (
+	"testing"
+
+	"github.com/segmentio/datastructures/v2/container/array"
+)
+
+var pool array.Pool[int]
+
+func TestQueue(t *testing.T) {
+	var q Queue[int]
+	count := (array.BlockLen[int]() * 5) - 10
+	t.Logf("using %d entries with block size %d", count, array.BlockLen[int]())
+
+	for i := 0; i < count; i++ {
+		if q.Len() != i {
+			t.Fatalf("incorrect count: expect=%d, got=%d", i, q.Len())
+		}
+		q.PushWithPool(i+1, &pool)
+	}
+
+	if q.Len() != count {
+		t.Fatalf("incorrect count: expect=%d, got=%d", count, q.Len())
+	}
+
+	c := q.values.Cap()
+
+	v, ok := q.PopWithPool(&pool)
+	if !ok {
+		t.Fatal("expected pop to remove value")
+	}
+	if v != 1 {
+		t.Fatalf("incorrected popped value: expect=%d, got=%d", 1, v)
+	}
+	if q.Len() != count-1 {
+		t.Errorf("incorrect count: expect=%d, got=%d", count-1, q.Len())
+	}
+	if q.values.Cap() != c {
+		t.Errorf("capacity should not have changed: expect=%d, got=%d", c, q.values.Cap())
+	}
+
+	at := 2
+	for q.Len() > 10 {
+		v, ok = q.PopWithPool(&pool)
+		if !ok {
+			t.Error("expected pop to remove value")
+		}
+		if v != at {
+			t.Fatalf("incorrected popped value: expect=%d, got=%d", at, v)
+		}
+		at += 1
+	}
+
+	if q.values.Cap() != array.BlockLen[int]() {
+		t.Errorf("capactiy should be one block: expect=%d, got=%d", array.BlockLen[int](), q.values.Cap())
+	}
+
+	for q.Len() > 0 {
+		before := q.Len()
+		v, ok = q.PopWithPool(&pool)
+		t.Logf("len = %d -> %d", before, q.Len())
+		if !ok {
+			t.Error("expected pop to remove value")
+		}
+		if v != at {
+			t.Fatalf("incorrected popped value: expect=%d, got=%d", at, v)
+		}
+		at += 1
+	}
+
+	if q.Len() != 0 {
+		t.Fatalf("incorrect count: expect=%d, got=%d", 0, q.Len())
+	}
+
+	if q.values.Cap() != array.BlockLen[int]() {
+		t.Errorf("capactiy should be one block: expect=%d, got=%d", array.BlockLen[int](), q.values.Cap())
+	}
+}
